40e8f1f9869f061b88ef15f9a5487ae1fb97ca68
ritter <ritter@62a0c1ba-02df-0310-a3dc-ad2bd7632c7a>
2004-08-02 03:40:33 +0000

    You can now double click a MOO in the MOO list to connect to it.

--- trunk/src/jmu/gui/dialog/MooListDialog.java
+++ trunk/src/jmu/gui/dialog/MooListDialog.java
@@ -5,6 +5,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ResourceBundle;
@@ -27,14 +29,16 @@ import org.xml.sax.SAXException;
 /**
  * @author dietzla
  */
-public class MooListDialog extends JDialog implements ActionListener {
+public class MooListDialog
+	extends JDialog
+	implements ActionListener, MouseListener {
 
 	private final ResourceBundle translations;
 
 	private final MainWindow main;
 
 	private final Moo[] moos;
-	
+
 	private final JList mooList;
 	private final JButton connect;
 	private final JButton cancel;
@@ -60,6 +64,7 @@ public class MooListDialog extends JDialog implements ActionListener {
 			}
 
 			mooList = new JList(mooNames);
+			mooList.addMouseListener(this);
 
 			connect = new JButton(translations.getString("Connect"));
 			connect.addActionListener(this);
@@ -68,10 +73,10 @@ public class MooListDialog extends JDialog implements ActionListener {
 			mooList = new JList();
 			connect = null;
 		}
-		
+
 		cancel = new JButton(translations.getString("Cancel"));
 		cancel.addActionListener(this);
-		
+
 		__layoutComponents();
 	}
 
@@ -80,22 +85,65 @@ public class MooListDialog extends JDialog implements ActionListener {
 	 */
 	public void actionPerformed(final ActionEvent ae) {
 		final Object source = ae.getSource();
-		
+
 		if (source == connect) {
-			final Moo selected = moos[mooList.getSelectedIndex()];
-		
-			final String address = selected.getAddresses()[0];
-			final int portSep = address.lastIndexOf(':');
-			final String host = address.substring(0, portSep);
-			final String port = address.substring(portSep + 1);
-		
-			dispose();
-			main.connect(host, port);
+			connect();
 		} else {
 			dispose();
 		}
 	}
-	
+
+	/**
+	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+	 */
+	public void mouseClicked(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() > 1) {
+			connect();
+		} else {
+			e.consume();
+		}
+	}
+
+	/**
+	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+	 */
+	public void mouseEntered(MouseEvent e) {
+		e.consume();
+	}
+
+	/**
+	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+	 */
+	public void mouseExited(MouseEvent e) {
+		e.consume();
+	}
+
+	/**
+	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+	 */
+	public void mouseReleased(MouseEvent e) {
+		e.consume();
+	}
+
+	/**
+	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+	 */
+	public void mousePressed(MouseEvent e) {
+		e.consume();
+	}
+
+	private void connect() {
+		final Moo selected = moos[mooList.getSelectedIndex()];
+
+		final String address = selected.getAddresses()[0];
+		final int portSep = address.lastIndexOf(':');
+		final String host = address.substring(0, portSep);
+		final String port = address.substring(portSep + 1);
+
+		dispose();
+		main.connect(host, port);
+	}
+
 	private Vector loadMooList()
 		throws IOException, ParserConfigurationException, SAXException {
 		SAXParserFactory parserFactory = SAXParserFactory.newInstance();
@@ -115,22 +163,24 @@ public class MooListDialog extends JDialog implements ActionListener {
 		}
 
 	}
-	
+
 	private void __layoutComponents() {
 		final Container pane = getContentPane();
 		pane.setLayout(new GridBagLayout());
-		
+
 		final GridBagConstraints c = new GridBagConstraints();
-		
-		c.gridy = 0; c.gridx = 0;
+
+		c.gridy = 0;
+		c.gridx = 0;
 		c.gridwidth = 2;
 		c.fill = GridBagConstraints.BOTH;
 		pane.add(new JScrollPane(mooList), c);
-		
-		c.gridy = 1; c.gridx = 0;
+
+		c.gridy = 1;
+		c.gridx = 0;
 		c.gridwidth = 1;
 		c.fill = GridBagConstraints.NONE;
-		
+
 		if (connect != null) {
 			pane.add(connect, c);
 
@@ -141,7 +191,7 @@ public class MooListDialog extends JDialog implements ActionListener {
 		}
 
 		pane.add(cancel, c);
-		
+
 		pack();
 	}
 
